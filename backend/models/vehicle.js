b7263a44c46d3e05fc4fe011360f72149e64b6c4
rajesh <rajesh@tatrasdata.com>
2020-01-13 15:22:59 +0530

    finished basic vehicle listing

---
+++ backend/models/vehicle.js
@@ -0,0 +1,145 @@
+const mongoose = require('mongoose');
+
+const vehicleSchema = new mongoose.Schema ({
+    vehicleID: {
+        type: Number
+    },
+    vehicle_type:{
+        type:String
+    },
+    vehicle_typeId:{
+        type:Number
+    },
+    vehicle_code: {
+        type: String
+    },
+    vehicleDetailsId: {
+        type: Number
+    },
+    name: {
+        type: String
+    },
+    yearofManufacturer: {
+        type: String
+    },
+    make: {
+        type: String
+    },
+    model: {
+        type: String
+    },
+    color: {
+        type: String
+    },
+    vehicleImage:{
+        type: String
+    },
+    regNo: {
+        type: String
+    },
+    engineNo :{
+        type: String
+    },
+    chassisNo : {
+        type: String
+    },
+    serialNo : {
+        type: String
+    },
+    warrantyPeriod : {
+        type: String
+    },
+    validity: {
+        type : String
+    },
+    fuelTypeId: {
+        type: String
+    },
+    fuelMeausrementId : {
+        type: String
+    },
+    trackUsageId : {
+        type: String
+    },
+    currentUser : {
+        type: String
+    },
+    currentUserDL: {
+        type: String
+    },
+    employeeID : {
+        type: String
+    },
+    vehicleOwnershipId: {
+        type: String
+    },
+    insuranceValid : {
+        type: String
+    },
+    insuranceNo : {
+        type: String
+    },
+    insuranceAmt : {
+        type: String
+    },
+    insuranceCopy : {
+        type: String
+    },
+    insuranceAgent : {
+        type: String
+    },
+    roadTaxValid : {
+        type: String
+    },
+    roadTaxNo : {
+        type: String
+    },
+    roadTaxAmt : {
+        type: String
+    },
+    roadTaxCopy : {
+        type: String
+    },
+    projectTypeID : {
+        type: String
+    },
+    projectID: {
+        type: String
+    },
+    vehicleStatusId: {
+        type: String
+    },
+    assignStatusId: {
+        type: String
+    },
+    workLocationId: {
+        type: String
+    },
+    qrCode: {
+        type: String
+    },
+    creatorId : {
+        type: String
+    },
+    createdDate : {
+        type: String
+    },
+    updatedDate : {
+        type: String
+    },
+    updateId: {
+        type: String
+    },
+    updatedStatus: {
+        type: String
+    },
+    organizationId : {
+        type: String
+    }
+
+
+});
+
+
+const Vehicle = mongoose.model("Vehicle", vehicleSchema, 'vehicle');
+exports.Vehicle = Vehicle;
