f1f582c3bd14412a86416748d951b10706c30d2b
amresh-web <amresh.web@gmail.com>
2020-03-24 00:40:01 +0530

    Fuel module create

---
+++ frontend/src/app/pages/fuel/fuel.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { FuelRoutingModule } from './fuel-routing.module';
+import { FuelComponent } from './fuel.component';
+import { ListComponent } from './list/list.component';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FuelRoutingModule,
+    FuelComponent
+  ],
+  declarations: [ListComponent],
+})
+export class FuelModule { }
